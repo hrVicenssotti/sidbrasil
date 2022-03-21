b683b4268d89fc4af82e1430c3368d50227b41b5
RawsenDead <horlancar.8@gmail.com>
2022-03-21 21:10:51 +0000

    Estrutura, imagens, e layout de entrada

---
+++ main.js
@@ -0,0 +1,12 @@
+const imgApresentacao = document.querySelector("#apresentacao>img")
+const textoApresentacao = document.querySelector("#apresentacao>p")
+let indexApresentacao = 0
+const imgsApresentacao = [
+    {
+        img: './image/dashbord/painel 1.png',
+        texto: 'Painel de navegação campleto e simples para usabilidade dos usuários, podendo ser construido conforme a necessidade de nossos clientes.'
+    }
+]
+setInterval(() => {
+
+}, 5000)
